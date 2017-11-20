10e65233edf1a5537671cf81c603fc6d271b2ec0
XiaofengWang <XiaofengWang@DESKTOP-NHQFNMJ>
2017-11-20 20:58:26 +0800

    no message

---
+++ csharp实例/依赖项注入/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 依赖项注入
+{
+    //打断和声明依赖项
+    public class Client
+    {
+        private IEmail duix;
+        public Client(IEmail duix2)
+        {
+            duix = duix2;
+        }
+
+        public string SendXiaoXi()
+        {
+            return duix.SendMessage();
+        }
+    }
+}
---
+++ csharp实例/依赖项注入/Email.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 依赖项注入
+{
+    public class Email:IEmail
+    {
+        public string SendMessage()
+        {
+            return "ssss";
+        }
+    }
+}
